97089b5ce0dc2c6ccc19b4a5800b8d5448e2fc80
bmavity <brian@brianmavity.com>
2014-05-12 01:19:23 -0500

    Length prefix message framer can now frame authenticated packets

--- bufftest.js
+++ bufftest.js
@@ -27,13 +27,13 @@ var net = require('net')
 var client = net.connect(1113, '127.0.0.1', function() { 
   console.log('client connected')
 			
-  sendAuthenticated('ReadAllEventsForward', parser.serialize('ReadAllEvents', {
+  sendMessage('ReadAllEventsForward', uuid.v4(), parser.serialize('ReadAllEvents', {
 		commit_position: 0
 	, prepare_position: 0
 	, max_count: 1000
 	, resolve_link_tos: false
 	, require_master: false
-	}))
+	}), true)
 })
 
 var _leftoverPacketData
@@ -84,41 +84,6 @@ function sendMessage(messageName, correlationId, payload, auth) {
 	var packet = framer.frame(messageName, correlationId, payload, auth)
 
   console.log("Sending " + messageName + " message with correlation id: " + correlationId)
-  
-  client.write(packet)
-}
 
-function sendAuthenticated(command, payload, correlation) {
-  var correlationId = correlation || uuid.v4()
-  	, payloadSize = payload ? payload.length : 0
-  	, dataOffset = 4
-  	, commandOffset = dataOffset
-  	, flagOffset = commandOffset + 1
-  	, correlationIdOffset = flagOffset + 1
-  	, authOffset = correlationIdOffset + 16
-  	, usernameLengthOffset = authOffset
-  	, usernameOffset = usernameLengthOffset + 1
-  	, usernameLength = Buffer.byteLength('admin')
-  	, passwordLengthOffset = usernameOffset + usernameLength
-  	, passwordOffset = passwordLengthOffset + 1
-  	, passwordLength = Buffer.byteLength('changeit')
-  	, payloadOffset = passwordOffset + passwordLength
-  	, contentLength = payloadOffset + payloadSize - dataOffset
-  	, packet = new Buffer(contentLength + dataOffset)
-
-  packet.writeUInt32LE(contentLength, 0)
-  packet.writeUInt8(commands(command), commandOffset)
-  packet.writeUInt8(flags('Authenticated'), flagOffset)
-  uuid.parse(correlationId, packet, correlationIdOffset)
-  packet.writeUInt8(usernameLength, usernameLengthOffset)
-  packet.write('admin', usernameOffset)
-  packet.writeUInt8(passwordLength, passwordLengthOffset)
-  packet.write('changeit', passwordOffset)
-
-  if (payloadSize > 0) {
-    payload.copy(packet, payloadOffset)
-  }
-
-  console.log("Sending " + command + " command with correlation id: " + correlationId)
   client.write(packet)
 }
--- lengthPrefixMessageFramer.js
+++ lengthPrefixMessageFramer.js
@@ -5,7 +5,7 @@ var uuid = require('node-uuid')
 	, commandOffset = lengthPrefixLength
 	, flagOffset = commandOffset + 1
 	, correlationIdOffset = flagOffset + 1
-	, payloadOffset = correlationIdOffset + 16
+	, authOffset = correlationIdOffset + 16
 
 module.exports = {
 	frame: frameMessage
@@ -13,14 +13,33 @@ module.exports = {
 
 
 function frameMessage(messageName, correlationId, payload, auth) {
-  var payloadSize = payload ? payload.length : 0
+  var usernameOffset = authOffset + 1
+  	, usernameLength = Buffer.byteLength('admin')
+  	, passwordLengthOffset = usernameOffset + usernameLength
+  	, passwordOffset = passwordLengthOffset + 1
+  	, passwordLength = Buffer.byteLength('changeit')
+  	, payloadOffset = !!auth ? passwordOffset + passwordLength : authOffset
+	  , payloadSize = payload ? payload.length : 0
 		, contentLength = payloadOffset + payloadSize - lengthPrefixLength
 		, packet = new Buffer(contentLength + lengthPrefixLength)
 
   packet.writeUInt32LE(contentLength, 0)
   packet.writeUInt8(messages(messageName), commandOffset)
-  packet.writeUInt8(flags('None'), flagOffset)
+
+  if(auth) {
+	  packet.writeUInt8(flags('Authenticated'), flagOffset)
+  } else {
+	  packet.writeUInt8(flags('None'), flagOffset)
+  }
+
   uuid.parse(correlationId, packet, correlationIdOffset)
+
+  if(auth) {
+	  packet.writeUInt8(usernameLength, authOffset)
+	  packet.write('admin', usernameOffset)
+	  packet.writeUInt8(passwordLength, passwordLengthOffset)
+	  packet.write('changeit', passwordOffset)
+	}
 
   if (payloadSize > 0) {
     payload.copy(packet, payloadOffset)
