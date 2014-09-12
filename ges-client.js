2fef5ea1e1b6d976fa4af03ff6fb6da1c7f0cedd
bmavity <brian@brianmavity.com>
2014-09-12 13:28:54 -0500

    Added a few create functions to module

--- ges-client.js
+++ ges-client.js
@@ -1,6 +1,11 @@
 var tcpConnect = require('./tcp/connection')
+	, streamMetadata = require('./tcp/streamMetadata')
+	, eventData = require('./eventData')
 	, expectedVersion = {}
 	, streamPosition = {}
+	, systemEventTypes = {}
+	, systemStreams = {}
+
 
 module.exports = createConnection
 
@@ -9,11 +14,10 @@ Object.defineProperties(module.exports, {
 	expectedVersion: { value: expectedVersion }
 , maxRecordCount: { value: 2147483647 }
 , streamPosition: { value: streamPosition }
-})
-
-Object.defineProperties(streamPosition, {
-	start: { value: 0 }
-, end: { value: -1 }
+, createEventData: { value: eventData }
+, createStreamMetadata: { value: streamMetadata }
+, systemEventTypes: { value: systemEventTypes }
+, systemStreams: { value: systemStreams }
 })
 
 Object.defineProperties(expectedVersion, {
@@ -22,7 +26,59 @@ Object.defineProperties(expectedVersion, {
 , emptyStream: { value: -1 }
 })
 
+Object.defineProperties(streamPosition, {
+	start: { value: 0 }
+, end: { value: -1 }
+})
+
+Object.defineProperties(systemEventTypes, {
+  streamDeleted: { value: '$streamDeleted' }
+, statsCollection: { value: '$statsCollected' }
+, linkTo: { value: '$>' }
+, streamMetadata: { value: '$metadata' }
+, settings: { value: '$settings' }
+})
+
+Object.defineProperties(systemStreams, {
+  streamsStream: { value: '$streams' }
+, settingsStream: { value: '$settings' }
+, statsStreamPrefix: { value: '$stats' }
+, metastreamOf: { value: function(streamId) {
+		return '$$' + streamId
+	}}
+, isMetastream: { value: function(metastreamId) {
+		return metastreamId.indexOf('$$') === 0
+	}}
+, originalStreamOf: { value: function(metastreamId) {
+		return metastreamId.slice(2)
+	}}
+})
+
 
 function createConnection(opts, cb) {
 	return tcpConnect(opts, cb)
 }
+
+/*
+    static class SystemMetadata
+    {
+        public const string MaxAge = "$maxAge";
+        public const string MaxCount = "$maxCount";
+        public const string TruncateBefore = "$tb";
+        public const string CacheControl = "$cacheControl";
+        
+        public const string Acl = "$acl";
+        public const string AclRead = "$r";
+        public const string AclWrite = "$w";
+        public const string AclDelete = "$d";
+        public const string AclMetaRead = "$mr";
+        public const string AclMetaWrite = "$mw";
+
+        public const string UserStreamAcl = "$userStreamAcl";
+        public const string SystemStreamAcl = "$systemStreamAcl";
+    }
+
+    internal static class SystemEventTypes
+    {
+    }
+ */
