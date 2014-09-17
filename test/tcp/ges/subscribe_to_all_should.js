1ff5235be5e04f759f0253c92d2060ac0e8acc1b
bmavity <brian@brianmavity.com>
2014-09-17 06:30:44 -0500

    Subscribe to all tests are migrated

--- test/tcp/ges/subscribe_to_all_should.js
+++ test/tcp/ges/subscribe_to_all_should.js
@@ -17,14 +17,100 @@ describe('subscribe_to_all_should', function() {
 			if(err) return done(err)
 
 			es = memory
-			connection = client({ port: 5023 }, done)
+			connection = client({ port: 5023 }, function(err) {
+				if(err) return done(err)
+					
+				var setData = {
+							expectedMetastreamVersion: client.expectedVersion.emptyStream
+						, metadata: client.createStreamMetadata({
+							  acl: {
+									readRoles: client.systemRoles.all
+								}
+							})
+						, auth: {
+								username: client.systemUsers.admin
+							, password: client.systemUsers.defaultAdminPassword
+							}
+						}
+
+				connection.setStreamMetadata('$all', setData, done)
+			})
 		})
 	})
 
-  it('allow_multiple_subscriptions')
-    //var stream = 'subscribe_to_all_should_allow_multiple_subscriptions'
-  it('catch_deleted_events_as_well')
-    //var stream = 'subscribe_to_all_should_catch_created_and_deleted_events_as_well'
+  it('allow_multiple_subscriptions', function(done) {
+    var stream = 'subscribe_to_all_should_allow_multiple_subscriptions'
+    	, sub1 = connection.subscribeToStream(stream)
+    	, sub2 = connection.subscribeToStream(stream)
+ 	    , appendData = {
+					expectedVersion: client.expectedVersion.emptyStream
+				, events: createTestEvent()
+		    } 
+    	, evtSub1
+    	, evtSub2
+
+    function testForFinish() {
+    	if(evtSub1 && evtSub2) {
+	    	true.should.be.true
+	    	done()
+    	}
+    }
+
+    sub1.on('event', function(evt) {
+    	evtSub1 = true
+    	testForFinish()
+    })
+
+    sub2.on('event', function(evt) {
+    	evtSub2 = true
+    	testForFinish()
+    })
+
+    connection.appendToStream(stream, appendData, function(err) {
+    	if(err) return done(err)
+    })
+  })
+
+  it('catch_deleted_events_as_well', function(done) {
+    var stream = 'subscribe_to_all_should_catch_created_and_deleted_events_as_well'
+    	, subscription = connection.subscribeToStream(stream)
+    	, callResults = {}
+
+    subscription.on('event', function(evt) {
+    	if(callResults.event) {
+    		should.fail()
+    	} else {
+    		callResults.event = true
+    		if(callResults.event && callResults.dropped) {
+    			should.pass()
+    			done()
+    		}
+    	}
+    })
+
+    subscription.on('dropped', function(evt) {
+    	if(callResults.dropped) {
+    		should.fail()
+    	} else {
+    		callResults.dropped = true
+    		if(callResults.event && callResults.dropped) {
+    			should.pass()
+    			done()
+    		}
+    	}
+    })
+
+    var deleteData = {
+    			expectedVersion: client.expectedVersion.emptyStream
+    		, hardDelete: true
+		    }
+
+    connection.deleteStream(stream, deleteData, function(err) {
+    	if(err) return done(err)
+
+	    subscription.unsubscribe()
+    })
+  })
 
   after(function(done) {
   	connection.close(function() {
