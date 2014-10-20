d0086ac48b3027a7b4df023c77a54ed8254639e2
bmavity <brian@brianmavity.com>
2014-10-19 21:25:56 -0500

    All transaction tests now passing

--- test/tcp/ges/appending_to_implicitly_created_stream_using_transaction.js
+++ test/tcp/ges/appending_to_implicitly_created_stream_using_transaction.js
@@ -21,26 +21,386 @@ describe('appending_to_implicitly_created_stream_using_transaction', function() 
 		})
 	})
 
-  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0em1_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0em1_idempotent'
-  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0any_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0any_idempotent'
-  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e5_non_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e5_non_idempotent'
-  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e6_wev')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e6_wev'
-  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e4_wev')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e4_wev'
-  it('sequence_0em1_0e0_non_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0e0_non_idempotent'
-  it('sequence_0em1_0any_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0any_idempotent'
-  it('sequence_0em1_0em1_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0em1_idempotent'
-  it('sequence_0em1_1e0_2e1_1any_1any_idempotent')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_1any_1any_idempotent'
-  it('sequence_S_0em1_1em1_E_S_0em1_1em1_2em1_E_idempotancy_fail')
-    //var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_S_0em1_1em1_E_S_0em1_1em1_2em1_E_idempotancy_fail'
+  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0em1_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0em1_idempotent'
+	    , allEvents = createTestEvent(range(0, 6))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(5)
+
+				  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(0)
+
+						 		eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(allEvents.length)
+							  	done()
+						 		})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0any_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0any_idempotent'
+	    , allEvents = createTestEvent(range(0, 6))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(5)
+
+				  connection.startTransaction(stream, { expectedVersion: client.expectedVersion.any }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(0)
+
+						 		eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(allEvents.length)
+							  	done()
+						 		})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e5_non_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e5_non_idempotent'
+	    , allEvents = createTestEvent(range(0, 6))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(5)
+
+				  connection.startTransaction(stream, { expectedVersion: 5 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(6)
+
+						 		eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(allEvents.length + 1)
+							  	done()
+						 		})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e6_wev', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e6_wev'
+	    , allEvents = createTestEvent(range(0, 6))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(5)
+
+				  connection.startTransaction(stream, { expectedVersion: 6 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	should.not.be.null(err)
+						  	err.message.should.equal('Wrong expected version.')
+						  	done()
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e4_wev', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_3e2_4e3_5e4_0e4_wev'
+	    , allEvents = createTestEvent(range(0, 6))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(5)
+
+				  connection.startTransaction(stream, { expectedVersion: 4 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	should.not.be.null(err)
+						  	err.message.should.equal('Wrong expected version.')
+						  	done()
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_0e0_non_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0e0_non_idempotent'
+	    , allEvents = createTestEvent(range(0, 1))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(0)
+
+				  connection.startTransaction(stream, { expectedVersion: 0 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(1)
+						  	eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(allEvents.length + 1)
+							  	done()
+						  	})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_0any_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0any_idempotent'
+	    , allEvents = createTestEvent(range(0, 1))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(0)
+
+				  connection.startTransaction(stream, { expectedVersion: client.expectedVersion.any }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(0)
+						  	eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(1)
+							  	done()
+						  	})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_0em1_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_0em1_idempotent'
+	    , allEvents = createTestEvent(range(0, 1))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(0)
+
+				  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[0], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	if(err) return done(err)
+
+						  	commitResult.NextExpectedVersion.should.equal(0)
+						  	eventStreamCounter(connection, stream, function(err, count) {
+							  	if(err) return done(err)
+
+							  	count.should.equal(1)
+							  	done()
+						  	})
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_0em1_1e0_2e1_1any_1any_idempotent', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_0em1_1e0_2e1_1any_1any_idempotent'
+	    , allEvents = createTestEvent(range(0, 3))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(2)
+
+				  connection.startTransaction(stream, { expectedVersion: client.expectedVersion.any }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents[1], function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.write(allEvents[1], function(err, writeResult) {
+						  	if(err) return done(err)
+
+							  transaction.commit(function(err, commitResult) {
+							  	if(err) return done(err)
+
+							  	commitResult.NextExpectedVersion.should.equal(1)
+							  	eventStreamCounter(connection, stream, function(err, count) {
+								  	if(err) return done(err)
+
+								  	count.should.equal(allEvents.length)
+								  	done()
+							  	})
+							  })
+						  })
+						})
+				  })
+			  })
+		  })
+	  })
+  })
+
+  it('sequence_S_0em1_1em1_E_S_0em1_1em1_2em1_E_idempotancy_fail', function(done) {
+    var stream = 'appending_to_implicitly_created_stream_using_transaction_sequence_S_0em1_1em1_E_S_0em1_1em1_2em1_E_idempotancy_fail'
+	    , allEvents = createTestEvent(range(0, 2))
+
+	  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+	  	if(err) return done(err)
+
+		  transaction.write(allEvents, function(err, writeResult) {
+		  	if(err) return done(err)
+
+			  transaction.commit(function(err, commitResult) {
+			  	if(err) return done(err)
+
+			  	commitResult.NextExpectedVersion.should.equal(1)
+
+				  connection.startTransaction(stream, { expectedVersion: -1 }, function(err, transaction) {
+				  	if(err) return done(err)
+
+					  transaction.write(allEvents.concat(createTestEvent()), function(err, writeResult) {
+					  	if(err) return done(err)
+
+						  transaction.commit(function(err, commitResult) {
+						  	should.not.be.null(err)
+						  	err.message.should.equal('Wrong expected version.')
+
+						  	done()
+						  })
+					  })
+				  })
+			  })
+		  })
+	  })
+  })
+
 
 
   after(function(done) {
