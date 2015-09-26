3de5aa8a8b26b113096a19bd03aecccd90c58ad5
bmavity <brian@brianmavity.com>
2015-09-26 08:59:53 -0500

    Event Data creation is now more user-friendly

--- eventData.js
+++ eventData.js
@@ -1,11 +1,19 @@
+var is = require('is')
+
 module.exports = function EventData(eventId, type, isJson, data, metadata) {
 	if(!(this instanceof EventData)) {
 		return new EventData(eventId, type, isJson, data, metadata)
 	}
 
 	isJson = !!isJson
-	data = data || new Buffer(0)
-	metadata = metadata || new Buffer(0)
+
+	if(!Buffer.isBuffer(data)) {
+		data = bufferize(isJson, data)
+	}
+
+	if(!Buffer.isBuffer(metadata)) {
+		metadata = bufferize(isJson, metadata)
+	}
 	
 	Object.defineProperties(this, {
 		EventId: { value: eventId, enumerable: true }
@@ -14,4 +22,25 @@ module.exports = function EventData(eventId, type, isJson, data, metadata) {
   , Data: { value: data, enumerable: true }
   , Metadata: { value: metadata, enumerable: true }
 	})
+}
+
+
+function bufferize(isJson, data) {
+	if(is['undefined'](data) || is['null'](data)) {
+		return new Buffer(0)
+	}
+
+	if(isJson && is.object(data)) {
+		return JSON.stringify(data)
+	}
+
+	if(is.object(data)) {
+		throw new Error('Cannot automatically create data or metadata Buffer for a non-json object, please supply the raw Buffer to the createEventData method instead or set isJson to true')
+	}
+
+	if(!is.string(data)) {
+		throw new Error('Cannot automatically create data or metadata Buffer for a non-string, please supply the raw Buffer to the createEventData method instead')
+	}
+
+	return new Buffer(data)
 }
